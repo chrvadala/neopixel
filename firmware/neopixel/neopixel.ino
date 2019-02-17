65d34d9a2553f350da5bc8096cb88ec6c22cc70f
Christian Vadalà <chr.vadala@gmail.com>
2019-02-17 13:50:53 +0100

    adds firmware libraries

--- firmware/neopixel/neopixel.ino
+++ firmware/neopixel/neopixel.ino
@@ -12,9 +12,9 @@
 
 **/
 
-#include <WiFiManager.h>
+#include "WiFiManager.h"
+#include "Adafruit_NeoPixel.h"
 #include <ESP8266mDNS.h>
-#include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
